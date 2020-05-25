d8b133ca286c9ccb75d6b5a608413f8584516b79
Marco Solorzano Baez <marsolbae@gmail.com>
2020-05-24 22:44:38 -0500

    1986

--- DataAnalysis.sql
+++ DataAnalysis.sql
@@ -1,10 +1,15 @@
--- 1. Salary by employee
+-- 1. Salary by employee.
 SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
 FROM employees e 
 	INNER JOIN salaries s
 		ON s.emp_no = e.emp_no
 	
--- 2. List employees who were hired in 1986.
+-- 2. Employees hired in 1986.
+SELECT * 
+FROM employees
+WHERE hire_date 
+	BETWEEN '1986-01-01' AND '1986-12-31'
+	
 -- 3. List the manager of each department with the following information: 
 -- department number, department name, the manager's employee number, last name, first name
 -- and start and end employment dates.
