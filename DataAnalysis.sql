acf39ea8388c2c2549cec94fb1dae62603abc61f
Marco Solorzano Baez <marsolbae@gmail.com>
2020-05-24 22:34:45 -0500

    Salary

--- DataAnalysis.sql
+++ DataAnalysis.sql
@@ -1,4 +1,9 @@
--- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
+-- 1. Salary by employee
+SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
+FROM employees e 
+	INNER JOIN salaries s
+		ON s.emp_no = e.emp_no
+	
 -- 2. List employees who were hired in 1986.
 -- 3. List the manager of each department with the following information: 
 -- department number, department name, the manager's employee number, last name, first name
