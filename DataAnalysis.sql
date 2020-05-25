208e2a425eda8d3cc90c10b30ba56a70a749c812
Marco Solorzano Baez <marsolbae@gmail.com>
2020-05-25 13:19:10 -0500

    Last name frequency

--- DataAnalysis.sql
+++ DataAnalysis.sql
@@ -41,7 +41,19 @@ FROM employees e
 		ON d.dept_no = de.dept_no
 WHERE dept_name = 'Sales'
 
--- 7. List all employees in the Sales and Development departments, including their employee number, 
--- last name, first name, and department name.
--- 8. In descending order, list the frequency count of employee last names, 
+-- 7. Sales and Development departments
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+	INNER JOIN dept_emp de 
+		ON de.emp_no = e.emp_no
+	INNER JOIN departments d
+		ON d.dept_no = de.dept_no
+WHERE dept_name = 'Sales' OR dept_name = 'Development' 
+
+-- 8. 
+-- In descending order, list the frequency count of employee last names, 
 -- i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS Count
+FROM employees
+GROUP BY last_name
+ORDER BY Count DESC
